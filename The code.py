4cdd1ec5263e480af54c8049c7d4ee68040ccd38
TeresaAng <TeresaAng@GithubUser.at>
2020-08-23 10:23:51 +0200

    correction of last commit

--- The code.py
+++ The code.py
@@ -38,19 +38,45 @@ while number == "No number":
                 number = generated_number
                 print ("Your number is", number)
                 break
-                
-number = round(float(number)) #???
-binumber = bin(number) # binär
-octnumber = oct(number) #oktale
-print("You entered the number", number, ":", binumber, "/", octnumber, ".")
 
+print("\nDo you know how this number is written in binary?")
+number = round(float(number))
+binumber = bin(number)                 
+   
+guess = input("Enter the number "+str(number)+" in binary: ")
+if guess == binumber:
+    print("correct!")
+else:
+    print("Sorry, wrong. The correct answer would be:", binumber)
 
+print("\nWhat about the octal number? Can you write it?")
+octanswer = input()
+octnumber = oct(number)
+if octanswer == octnumber:
+    print("correct!")
+else:
+    print("Sorry, wrong. The correct answer would be:", octnumber)
 
+print("\nNow, let us practise some simple math. Do you want to pick the numbers yourself?" 
+      " y or n?")
+pickYourself = input("You said: ") or "No answer"
+if pickYourself == "y":
+    try:
+        number_1 = float(input("Enter the first number: "))
+    except:
+        print("You didn't enter a number, that is why your number will be picked for you")
+        number_1 = random.randint(0,100)
+    try:
+        number_2 = float(input("Enter the second number: "))
+    except:
+        print("You didn't enter a number, that is why your number will be picked for you")        
+        number_2 = random.randint(0,100)
+else:
+    number_1 = random.randint(0,100)
+    number_2 = random.randint(0,100)
 
-number_1 = int(binumber[2:])
-number_2 = int(octnumber[2:])
-print("The results of the above numbers are:", number_1, "and", number_2)
-operations_list = ["addition", "subtraction", "multiplication", "division"]
+print("The numbers are:", number_1, "and", number_2)
+operations_list = ["\naddition", "subtraction", "multiplication", "division"]
 symbols_list = ["+", "-", "*", "/"]
 calculate_list = [number_1+number_2, number_1-number_2, number_1*number_2, number_1/number_2]
 for operations_list, symbols_list, calculate_list in zip(operations_list, 
@@ -61,4 +87,4 @@ for operations_list, symbols_list, calculate_list in zip(operations_list,
 
 
 
-
+   
